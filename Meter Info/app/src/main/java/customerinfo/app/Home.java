4230f5c97d203a5ad99477a0015447e76125b135
jasimbpdb2 <jasimbpdb2@gmail.com>
2025-11-20 21:31:13 +0600

    Update Home.java

--- Meter Info/app/src/main/java/customerinfo/app/Home.java
+++ Meter Info/app/src/main/java/customerinfo/app/Home.java
@@ -9,14 +9,14 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        
+
         Button lookupBtn = findViewById(R.id.lookupBtn);
-        Button applicationBtn = findViewById(R.id.applicationBtn); // CHANGED FROM htmlBtn
-        
+        Button applicationBtn = findViewById(R.id.applicationBtn);
+
         lookupBtn.setOnClickListener(v -> {
-            startActivity(new Intent(Home.this, MainActivity.class));
+            startActivity(new Intent(Home.this, LookupHtmlActivity.class));
         });
-        
+
         applicationBtn.setOnClickListener(v -> {
             startActivity(new Intent(Home.this, HtmlActivity.class));
         });
